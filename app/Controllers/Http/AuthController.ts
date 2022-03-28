a0ff1c2ef75e35a3e73365ccdbefd5abc551de10
Nathael Bonnal <kirua48000@gmail.com>
2022-03-28 04:00:50 +0200

    :construction: create AuthController

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,27 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+
+
+  public async login ({view}: HttpContextContract) {
+    return view.render('auth/login')
+  }
+
+  public async loginWeb ({ request, auth, response, session}: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      await auth.use('web').attempt(email, password)
+      await response.redirect().toRoute('home')
+    } catch  {
+      session.flash({error: "Identifiants Incorrects"})
+      response.redirect().toRoute('login')
+    }
+  }
+
+  public async logoutWeb ({ auth, response }: HttpContextContract) {
+    await auth.use('web').logout()
+    return response.redirect().toRoute('home')
+  }
+}
