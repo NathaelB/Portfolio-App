1176859cbf4e79b89428234caa00bc73ece0ec9f
Nathael Bonnal <kirua48000@gmail.com>
2022-04-05 02:49:04 +0200

    :construction: create migrations

---
+++ database/migrations/1648741373220_achievements.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Achievements extends BaseSchema {
+  protected tableName = 'achievements'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.string('id').primary()
+      table.string('title')
+      table.string('slug')
+      table.string('description')
+      table.string('category')
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
