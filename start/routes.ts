316ca24e073f87def53297d0b39b8b1be940e07f
Nathael Bonnal <kirua48000@gmail.com>
2022-04-05 02:39:59 +0200

    :construction: add route

--- start/routes.ts
+++ start/routes.ts
@@ -20,15 +20,21 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-
 Route.get('/', async ({ view }) => {
   return view.render('pages/index')
 }).as('home')
+
 Route.get('/notion', 'BlogsController.show')
 Route.get('/about-me', async ({view}) => {
   return view.render('pages/about')
-})
+}).as('about')
 
+Route.get('/achievements', 'AchievementsController.get').as('achievements')
+Route.get('/achievements/:id', 'AchievementsController.visit').as('achievement')
+Route.get('/blogs', 'BlogsController.get').as('blogs').as('blogs')
+Route.get('/contact', async ({view}) => {
+  return view.render('pages/index')
+}).as('contact')
 Route.group(() => {
   Route.get('/login', 'AuthController.login').as('login')
   Route.post('/login', 'AuthController.loginWeb')
@@ -41,7 +47,14 @@ Route.group(() => {
 
 Route.group(() => {
   Route.get('/', 'ManagersController.show')
-  Route.get('/users', 'UsersController.index')
+  Route.get('/users', 'UsersController.index').as('manager.user')
+
+  Route.get('/achievements', 'AchievementsController.index').as('manager.achievements')
+  Route.get('/achievements/new', 'AchievementsController.create').as('manager.achievements.create')
+  Route.post('/achievements/new', 'AchievementsController.store')
+  Route.get('/achievements/:id', 'AchievementsController.show').as('manager.achievement')
+  Route.delete('/achievements/:id', 'AchievementsController.destroy')
+
 }).prefix('manager')
 
 Route.get('/test', async ({view, response, bouncer}) => {
