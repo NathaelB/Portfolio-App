d699803767ed03bd67003b64dd7087128520915d
Nathael Bonnal <kirua48000@gmail.com>
2022-04-12 08:30:31 +0200

    :construction: create method `create` & update `store`, `index`

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,13 +3,16 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import StoreValidator from 'App/Validators/user/StoreValidator'
 import UpdateValidator from 'App/Validators/user/UpdateValidator'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UsersController {
 
-  public async index ({ view, auth, bouncer, response }: HttpContextContract) {
-    const users = await User.all()
+  public async index ({ view, auth, bouncer, response, request }: HttpContextContract) {
+    const page = request.input('page', 1)
+    const users = await Database.from(User.table).paginate(page, 5)
+    //console.log(users.getUrlsForRange(1, users.lastPage))
     if (await bouncer.can('view', auth.user)) {
-      return view.render('pages/manager/user', {
+      return view.render('pages/manager/users/index', {
         users
       })
     }
@@ -22,9 +25,31 @@ export default class UsersController {
       .first()
   }
 
-  public async store ({ request }: HttpContextContract): Promise<User> {
-    const data = await request.validate(StoreValidator)
-    return User.create(data)
+  public async create ({ view, auth, bouncer, response}: HttpContextContract) {
+    if (await bouncer.can('view', auth.user)) {
+      const user = new User()
+      return view.render('pages/manager/users/create', {
+        user
+      })
+    }
+    return response.redirect().toRoute('home')
+  }
+
+  public async store ({ request, bouncer, auth, response, session }: HttpContextContract) {
+    if (await bouncer.can('view', auth.user)) {
+      const data = await request.validate(StoreValidator)
+      await User.create({
+        ...data,
+      })
+
+      session.flash({
+        success: "Utilisateur Créer"
+      })
+
+      return response.redirect().toRoute('manager.users')
+    }
+    return response.redirect().toRoute('home')
+
   }
 
   public async update ({ request, params, response }: HttpContextContract) {
