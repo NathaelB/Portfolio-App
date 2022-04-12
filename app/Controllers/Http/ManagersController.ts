b81cade6276061bc29c9e3a4a309675b1db3d118
Nathael Bonnal <kirua48000@gmail.com>
2022-04-12 08:29:45 +0200

    :construction: add var `user`

--- app/Controllers/Http/ManagersController.ts
+++ app/Controllers/Http/ManagersController.ts
@@ -4,7 +4,9 @@ export default class ManagersController {
 
   public async show ({ view, auth, bouncer, response }: HttpContextContract) {
     if (await bouncer.can('view', auth.user)) {
-      return view.render('pages/manager/index')
+      return view.render('pages/manager/index', {
+        user: auth.user
+      })
     }
     return response.redirect().toRoute('home')
   }
