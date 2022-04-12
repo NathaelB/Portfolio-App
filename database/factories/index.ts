b1f9a5efacd2a45f1ab78aed61265d0fedd72cdf
Nathael Bonnal <kirua48000@gmail.com>
2022-04-12 08:33:16 +0200

    :sparkles: create new Factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,5 +1,6 @@
 import Factory, { FactoryContextContract } from '@ioc:Adonis/Lucid/Factory'
 import User from 'App/Models/User'
+import Achievement from 'App/Models/Achievement'
 
 export const UserFactory = Factory
   .define(User, ({ faker }: FactoryContextContract) => {
@@ -7,6 +8,16 @@ export const UserFactory = Factory
       username: faker.internet.userName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
+      access: false
     }
   }).build()
 
+
+export const AchievementFactory = Factory
+  .define(Achievement, ({faker}: FactoryContextContract) => {
+    return {
+      title: faker.company.companyName(),
+      description: faker.hacker.phrase(),
+      category: 'WEBSITE'
+    }
+  }).build()
